f1e087842e12b2a56266e5a2d81fc313e8b3c3f1
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-22 19:22:02 +0800

    Updated Canvas component to include undo/redo functionalities with a state management system

--- components/Canvas/Canvas.tsx
+++ components/Canvas/Canvas.tsx
@@ -1,18 +1,43 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 
 import { CanvasProps } from '@/types';
+import { CanvasMode, CanvasState } from '@/types/canvas';
+import { useHistory, useCanUndo, useCanRedo } from '@liveblocks/react/suspense';
 
 import Info from './Info';
 import Participants from './Participants';
 import Toolbar from './Toolbar';
 
 export default function Canvas({ boardId }: CanvasProps) {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory(),
+    canUndo = useCanUndo(),
+    canRedo = useCanRedo();
+
+  const handleUndo = () => {
+    history.undo();
+  };
+
+  const handleRedo = () => {
+    history.redo();
+  };
+
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none'>
-      <Info />
+      <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canUndo={canUndo}
+        canRedo={canRedo}
+        undo={handleUndo}
+        redo={handleRedo}
+      />
     </main>
   );
 }
