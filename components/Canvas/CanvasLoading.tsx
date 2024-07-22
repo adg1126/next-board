ff37d5d9a86262a1f6b74b6ebe257639db43b1e9
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-22 19:24:22 +0800

    Created Loader for Canvas component

--- components/Canvas/CanvasLoading.tsx
+++ components/Canvas/CanvasLoading.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 
 import { Loader } from 'lucide-react';
-import Info from '@/components/Canvas/Info';
-import Participants from '@/components/Canvas/Participants';
-import Toolbar from './Toolbar';
+import { InfoSkeleton } from '@/components/Canvas/Info';
+import { ParticipantsSkeleton } from '@/components/Canvas/Participants';
+import { ToolbarSkeleton } from './Toolbar';
 
 export default function CanvasLoading() {
   return (
     <main className='h-full w-full relative bg-neutral-100 touch-none flex items-center justify-center'>
       <Loader className='h-6 w-6 text-muted-foreground animate-spin' />
-      <Info.Skeleton />
-      <Participants.Skeleton />
-      <Toolbar.Skeleton />
+      <InfoSkeleton />
+      <ParticipantsSkeleton />
+      <ToolbarSkeleton />
     </main>
   );
 }
