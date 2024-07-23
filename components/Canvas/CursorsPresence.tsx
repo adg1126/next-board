fa680b12026c351b739b5f6a784abb91a91b6c9a
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-23 23:15:50 +0800

    Implemented functionality that broadcasts other teammates drawing on the same board

--- components/Canvas/CursorsPresence.tsx
+++ components/Canvas/CursorsPresence.tsx
@@ -2,11 +2,18 @@
 
 import React, { memo } from 'react';
 
-import { useOther, useOthersConnectionIds } from '@liveblocks/react/suspense';
+import {
+  shallow,
+  useOther,
+  useOthersConnectionIds,
+  useOthersMapped,
+} from '@liveblocks/react/suspense';
 import { CursorProps } from '@/types';
 import { MousePointer2 } from 'lucide-react';
 
-import { connectionIdToColor } from '@/lib/utils';
+import { colorToCss, connectionIdToColor } from '@/lib/utils';
+
+import Path from '../LayerComponents/Path';
 
 const Cursor = memo(({ connectionId }: CursorProps) => {
   const info = useOther(connectionId, (user) => user?.info),
@@ -46,7 +53,8 @@ const Cursor = memo(({ connectionId }: CursorProps) => {
 
 Cursor.displayName = 'Cursor';
 
-const Cursors = () => {
+// Broadcasts other users cursors when they are in the same board
+const OtherPeopleCursors = () => {
   const ids = useOthersConnectionIds();
 
   return (
@@ -61,11 +69,38 @@ const Cursors = () => {
   );
 };
 
+// Broadcasts other users drawing when they are in the same board
+const OtherPeopleDrafts = () => {
+  const others = useOthersMapped(
+    (other) => ({
+      pencilDraft: other.presence.pencilDraft,
+      penColor: other.presence.penColor,
+    }),
+    shallow
+  );
+
+  return (
+    <>
+      {others.map(([key, other]) =>
+        other.pencilDraft ? (
+          <Path
+            key={key}
+            x={0}
+            y={0}
+            points={other.pencilDraft}
+            fill={other.penColor ? colorToCss(other.penColor) : '#000'}
+          />
+        ) : null
+      )}
+    </>
+  );
+};
+
 export const CursorsPresence = memo(() => {
   return (
     <>
-      {/* TODO: Draft pencil */}
-      <Cursors />
+      <OtherPeopleCursors />
+      <OtherPeopleDrafts />
     </>
   );
 });
