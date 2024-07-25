a4fa2df3338a3fa473c45daf3e74cabe7a646190
adg1126 <gutierrez.almiradyan@gmail.com>
2024-07-25 11:40:51 +0800

    Changed favicon ico and title for each page

--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useEffect } from 'react';
 
 import { BoardPageProps } from '@/types';
 
@@ -6,7 +7,21 @@ import Canvas from '@/components/Canvas/Canvas';
 import Room from '@/components/Room';
 import CanvasLoading from '@/components/Canvas/CanvasLoading';
 
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { Id } from '@/convex/_generated/dataModel';
+
 export default function Board({ params }: BoardPageProps) {
+  const data = useQuery(api.board.get, {
+    id: params.boardId as Id<'boards'>,
+  });
+
+  useEffect(() => {
+    if (data) {
+      document.title = data.title;
+    }
+  }, [data]);
+
   return (
     <Room
       roomId={params.boardId}
